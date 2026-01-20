352e73b05730db1ec98bfc27b555230d8934d9d0
Karan Vishwakarma <karan@Karans-MacBook-Air.local>
2026-01-19 19:52:32 -0800

    linear

--- lg.py
+++ lg.py
@@ -1,12 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import numpy
 
 torch.manual_seed(42)
 
-X = torch.randn(100,1)
-Y = 3 * X + 2 * 0.1 * torch.randn(100,1)
+X= torch.randn(100,1)
+Y = 3 * X + 2 + 0.1 * torch.randn(100, 1)  # True relationship with noise
+
 
 class LinearRegression(nn.Module):
     def __init__(self):
@@ -16,21 +16,20 @@ class LinearRegression(nn.Module):
     def forward(self,x):
         return self.linear(x)
 
+
 model = LinearRegression()
-
-
 criterion = nn.MSELoss()
-optimzer = optim.SGD(model.parameters(),lr=0.01)
+optimizer = optim.SGD(model.parameters(),lr = 0.01)
 
-epochs = 1000
-for epoch in range(0,epochs):
-    prediction = model(X)
-    loss = criterion(prediction,Y)
-    optimzer.zero_grad()
+epochs = 3000
+for epoch in range(1,epochs):
+    predictions = model(X)
+    loss = criterion(predictions,Y)
+    optimizer.zero_grad()
     loss.backward()
-    optimzer.step()
-    if (epoch + 1) % 100 == 0:
-        print(f"Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.4f}")
+    optimizer.step()
+    if(epoch +1) %100 == 0:
+        print(f"Epoch [{(epoch+1)/epochs}], Loss:{loss}")
 
 weight = model.linear.weight.item()
 bias = model.linear.bias.item()
